e7752703c9e657bc47768b8e1300232873390dcc
Sarvinoz0112 <maxmadaliyevasarvinoz@gmail.com>
2025-02-28 14:57:00 +0500

    first commit

---
+++ app_movies/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+
+from app_movies import views
+from app_movies.views import CommentDetail, CommentListApiView
+
+app_name = 'movies'
+
+urlpatterns = [
+    path("",views.MovieList.as_view(),name="movie_list"),
+    path("<int:pk>/",views.MovieDetail.as_view(),name="movie_detail"),
+    path("<int:pk>/add_actor/", views.MovieDetail.as_view(), name="add_actor"),
+
+    path("actor/",views.ActorList.as_view(),name="actor_list"),
+    path("actor/<int:pk>/",views.ActorDetail.as_view(),name="actor_detail"),
+
+    path('<int:movie_id>/comments/', CommentListApiView.as_view(), name='movie-comments-list'),
+    path('comments/<int:pk>/', CommentDetail.as_view(), name='movie-comments-detail'),
+]
---
+++ app_users/views.py
@@ -0,0 +1,122 @@
+from asyncio import timeout
+from random import randint
+
+from django.core.cache import cache
+from drf_yasg.utils import swagger_auto_schema
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework_simplejwt.tokens import RefreshToken
+from yaml import serialize
+
+from .models import User
+from .serializers import VerifyOTPSerializer, RegisterSerializer, PhoneSerializer
+
+
+class PhoneAPIView(APIView):
+    @swagger_auto_schema(request_body=PhoneSerializer)
+    def post(self, request):
+        serializer = PhoneSerializer(data=request.data)
+        if serializer.is_valid():
+            phone = serializer.validated_data['phone']
+
+            otp_code = str(randint(1000, 9999))
+            print("Yaratilgan OTP:", otp_code)
+
+            cache.set(phone, {"otp": otp_code, "phone_number": phone}, timeout=600)
+
+            return Response(
+                {"success":True,"detail":"Sizga kod yuborildi!"},
+                status=status.HTTP_201_CREATED
+            )
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class VerifyOTPAPIView(APIView):
+        @swagger_auto_schema(request_body=VerifyOTPSerializer)
+        def post(self, request):
+            serializer = VerifyOTPSerializer(data=request.data)
+            if serializer.is_valid():
+                phone = serializer.validated_data['phone']
+                verification_code = serializer.validated_data['verification_code']
+                cached_otp = cache.get(phone)
+
+                if str(cached_otp.get("otp")) == str(verification_code):
+
+                    return Response(
+                        {"success":True,"detail":"OTP tasdiqlandi. Endi ro'yxatdan o'tishingiz mumkin."},
+                        status=status.HTTP_200_OK
+                    )
+
+                return Response(
+                    {"success":False,"detail":"Invalid phone number or OTP code."},
+                     status=status.HTTP_400_BAD_REQUEST
+                )
+
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class RegisterAPIView(APIView):
+    @swagger_auto_schema(request_body=RegisterSerializer)
+    def post(self, request):
+        serializer = RegisterSerializer(data=request.data)
+        if serializer.is_valid():
+
+            phone = serializer.validated_data['phone']
+            cached_data = cache.get(phone)
+
+            if not cached_data:
+                return Response(
+                    {"success": False, "detail": "Your phone number is invalid."},
+                )
+
+            phone_number = cached_data.get("phone_number")
+
+            if str(phone_number) == str(phone):
+                serializer.save()
+                return Response(
+                    {"success": True, "detail": "You registered successfully!"},
+                    status=status.HTTP_201_CREATED
+                )
+
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class LoginAPIView(APIView):
+    @swagger_auto_schema(request_body=RegisterSerializer)
+    def post(self, request):
+        phone = request.data.get("phone")
+        password = request.data.get("password")
+
+        user = User.objects.get(phone=phone).first()
+
+        if user and user.check_password(password):
+            refresh = RefreshToken.for_user(user)
+            return Response({
+                'refresh': str(refresh),
+                'access': str(refresh.access_token),
+                'is_admin': user.is_admin,
+                'is_user': user.is_user,
+                'is_staff': user.is_staff,
+            })
+
+        return Response({"error"}, status=status.HTTP_401_UNAUTHORIZED)
+
+class ProfileAPIView(APIView):
+    permission_classes = [IsAuthenticated]
+    def get(self, request):
+        serializer = RegisterSerializer(request.user)
+
+        return Response(
+            {"success":True,"data":serializer.data},
+            status=status.HTTP_200_OK
+        )
+
+    def patch(self, request):
+        serializer = RegisterSerializer(request.user, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(
+                {"success":True,"data":serializer.data},
+                status=status.HTTP_201_CREATEDOK
+            )
